6c0da0f966b2a786f0a3d18478fe15087ec20dfa
Allan Hung (DCS-TW) <allan_hung@tzude-mbp.client.tw.trendnet.org>
2018-11-08 13:39:15 +0800

    fix with oplog disable

--- mongobackup/backup.go
+++ mongobackup/backup.go
@@ -6,30 +6,31 @@
 **
 ** Started on  Wed 23 Dec 17:39:06 2015 gaspar_d
 ** Last update Mon  7 Mar 16:52:38 2016 gaspar_d
-*/
+ */
 
 package mongobackup
 
 import (
-  "os"
-  "time"
-  "strconv"
-  "gopkg.in/mgo.v2/bson"
+	"os"
+	"strconv"
+	"time"
+
+	"gopkg.in/mgo.v2/bson"
 )
 
 // perform an incremental or full backup
 // check the documentation of the performFullBackup, perforIncrementalBackup
 // for more information
 func (e *BackupEnv) PerformBackup() {
-  backupId         := strconv.Itoa(e.homeval.content.Sequence)
-  e.backupdirectory = e.Options.Directory + "/" + backupId;
-  e.ensureSecondary();
+	backupId := strconv.Itoa(e.homeval.content.Sequence)
+	e.backupdirectory = e.Options.Directory + "/" + backupId
+	e.ensureSecondary()
 
-  if (! e.Options.Incremental) {
-    e.performFullBackup(backupId);
-  } else {
-    e.perforIncrementalBackup(backupId);
-  }
+	if !e.Options.Incremental {
+		e.performFullBackup(backupId)
+	} else {
+		e.perforIncrementalBackup(backupId)
+	}
 }
 
 // perform a full backup
@@ -37,70 +38,75 @@ func (e *BackupEnv) PerformBackup() {
 // by default will lock the db with fsyncLock
 // will perform a rs.stepDown() if the node is primary
 func (e *BackupEnv) performFullBackup(backupId string) {
-  newEntry := BackupEntry{}
-  e.fetchDBPath();
-  e.info.Printf("Performing full backup of: %s", e.dbpath);
+	newEntry := BackupEntry{}
+	e.fetchDBPath()
+	e.info.Printf("Performing full backup of: %s", e.dbpath)
 
-  if (e.Options.Fsynclock) {
-    e.info.Printf("Locking the database")
-    if(e.mongoFsyncLock() != nil) {
-      e.CleanupBackupEnv();
-      os.Exit(1);
-    }
-  }
+	if e.Options.Fsynclock {
+		e.info.Printf("Locking the database")
+		if e.mongoFsyncLock() != nil {
+			e.CleanupBackupEnv()
+			os.Exit(1)
+		}
+	}
 
-  /* Begining critical path */
-  err, size := e.CopyDir(e.dbpath, e.backupdirectory);
-  sizeGb    := float64(size) / (1024*1024*1024);
-  if (err != nil) {
-    e.error.Print("An error occurred while backing up ...");
-    e.CleanupBackupEnv();
-    os.Exit(1);
-  }
+	/* Begining critical path */
+	// err, size := e.CopyDir(e.dbpath, e.backupdirectory);
+	err, size := e.TarDir(e.dbpath, e.backupdirectory)
+	sizeGb := float64(size) / (1024 * 1024 * 1024)
+	if err != nil {
+		e.error.Print("An error occurred while backing up ...")
+		e.CleanupBackupEnv()
+		os.Exit(1)
+	}
 
-  /* Dumping oplog for PIT recovery */
-  firstOplogEntries := e.getOplogFirstEntries()["ts"].(bson.MongoTimestamp)
+	/* Dumping oplog for PIT recovery */
+	if len(e.getOplogFirstEntries()) == 0 {
+		newEntry.LastOplog = 0
+		newEntry.FirstOplog = 0
+	} else {
+		firstOplogEntries := e.getOplogFirstEntries()["ts"].(bson.MongoTimestamp)
 
-  if (firstOplogEntries > e.homeval.lastOplog) {
-    e.warning.Printf("Can not find a common point in the oplog")
-    e.warning.Printf("point in time restore is not available before this backup")
-    newEntry.LastOplog   = e.getOplogLastEntries()["ts"].(bson.MongoTimestamp)
-    newEntry.FirstOplog  = firstOplogEntries
-  } else {
-    cursor            := e.getOplogEntries(e.homeval.lastOplog)
-    err, _, fop, lop  := e.BackupOplogToDir(cursor, e.backupdirectory)
+		if firstOplogEntries > e.homeval.lastOplog {
+			e.warning.Printf("Can not find a common point in the oplog")
+			e.warning.Printf("point in time restore is not available before this backup")
+			newEntry.LastOplog = e.getOplogLastEntries()["ts"].(bson.MongoTimestamp)
+			newEntry.FirstOplog = firstOplogEntries
+		} else {
+			cursor := e.getOplogEntries(e.homeval.lastOplog)
+			err, _, fop, lop := e.BackupOplogToDir(cursor, e.backupdirectory)
 
-    if (err != nil) {
-      e.error.Printf("Error while dumping oplog to %s (%s)", e.backupdirectory, err)
-      e.CleanupBackupEnv()
-      os.Exit(1)
-    }
+			if err != nil {
+				e.error.Printf("Error while dumping oplog to %s (%s)", e.backupdirectory, err)
+				e.CleanupBackupEnv()
+				os.Exit(1)
+			}
 
-    newEntry.LastOplog       = lop
-    newEntry.FirstOplog      = fop
-  }
+			newEntry.LastOplog = lop
+			newEntry.FirstOplog = fop
+		}
+	}
 
-  newEntry.Id     = backupId
-  newEntry.Ts     = time.Now()
-  newEntry.Source = e.dbpath
-  newEntry.Dest   = e.backupdirectory
-  newEntry.Kind   = e.Options.Kind
-  newEntry.Type   = "full"
-  newEntry.Compress        = e.Options.Compress
-  e.homeval.AddNewEntry(newEntry)
-  e.homeval.Flush()
+	newEntry.Id = backupId
+	newEntry.Ts = time.Now()
+	newEntry.Source = e.dbpath
+	newEntry.Dest = e.backupdirectory
+	newEntry.Kind = e.Options.Kind
+	newEntry.Type = "full"
+	newEntry.Compress = e.Options.Compress
+	e.homeval.AddNewEntry(newEntry)
+	e.homeval.Flush()
 
+	e.info.Printf("Success, %fGB of data has been saved in %s", sizeGb, e.backupdirectory)
 
-  e.info.Printf("Success, %fGB of data has been saved in %s", sizeGb, e.backupdirectory);
-
-  /* End of critical path */
-  if (e.Options.Fsynclock) {
-    e.info.Printf("Unlocking the database")
-    if (e.mongoFsyncUnLock() != nil) {
-      e.CleanupBackupEnv();
-      os.Exit(1);
-    }
-  }
+	/* End of critical path */
+	if e.Options.Fsynclock {
+		e.info.Printf("Unlocking the database")
+		if e.mongoFsyncUnLock() != nil {
+			e.CleanupBackupEnv()
+			os.Exit(1)
+		}
+	}
 }
 
 // perform an incremental backup
@@ -108,52 +114,52 @@ func (e *BackupEnv) performFullBackup(backupId string) {
 // if a common point in the oplog can not be found, a
 // full backup has to be performed
 func (e *BackupEnv) perforIncrementalBackup(backupId string) {
-  var (
-    lastSavedOplog    bson.MongoTimestamp
-    firstOplogEntries bson.MongoTimestamp
-  )
+	var (
+		lastSavedOplog    bson.MongoTimestamp
+		firstOplogEntries bson.MongoTimestamp
+	)
 
-  e.info.Printf("Performing incremental backup of: %s", e.Options.Mongohost);
+	e.info.Printf("Performing incremental backup of: %s", e.Options.Mongohost)
 
-  lastSavedOplog    = e.homeval.lastOplog;
-  firstOplogEntries = e.getOplogFirstEntries()["ts"].(bson.MongoTimestamp);
+	lastSavedOplog = e.homeval.lastOplog
+	firstOplogEntries = e.getOplogFirstEntries()["ts"].(bson.MongoTimestamp)
 
-  if (firstOplogEntries > lastSavedOplog) {
-    e.error.Printf("Can not find a common point in the oplog");
-    e.error.Printf("You must perform a full backup");
+	if firstOplogEntries > lastSavedOplog {
+		e.error.Printf("Can not find a common point in the oplog")
+		e.error.Printf("You must perform a full backup")
 
-    e.CleanupBackupEnv()
-    os.Exit(1);
-  }
+		e.CleanupBackupEnv()
+		os.Exit(1)
+	}
 
-  cursor               := e.getOplogEntries(lastSavedOplog)
-  err, size, fop, lop  := e.BackupOplogToDir(cursor, e.backupdirectory)
+	cursor := e.getOplogEntries(lastSavedOplog)
+	err, size, fop, lop := e.BackupOplogToDir(cursor, e.backupdirectory)
 
-  if (err != nil) {
-    e.error.Printf("Error while dumping oplog to %s (%s)", e.backupdirectory, err)
-    e.CleanupBackupEnv()
-    os.Exit(1)
-  }
+	if err != nil {
+		e.error.Printf("Error while dumping oplog to %s (%s)", e.backupdirectory, err)
+		e.CleanupBackupEnv()
+		os.Exit(1)
+	}
 
-  firstOplogEntries = e.getOplogFirstEntries()["ts"].(bson.MongoTimestamp);
-  if firstOplogEntries > lastSavedOplog {
-    e.warning.Printf("Possible gap in the oplog, last known entry has been reached during the operation")
-    e.warning.Printf("if this message appears often, please consider increasing the oplog size")
-    e.warning.Printf("https://docs.mongodb.org/manual/tutorial/change-oplog-size/")
-  }
+	firstOplogEntries = e.getOplogFirstEntries()["ts"].(bson.MongoTimestamp)
+	if firstOplogEntries > lastSavedOplog {
+		e.warning.Printf("Possible gap in the oplog, last known entry has been reached during the operation")
+		e.warning.Printf("if this message appears often, please consider increasing the oplog size")
+		e.warning.Printf("https://docs.mongodb.org/manual/tutorial/change-oplog-size/")
+	}
 
-  newEntry       := BackupEntry{}
-  newEntry.Id     = backupId
-  newEntry.Ts     = time.Now()
-  newEntry.Source = e.Options.Mongohost
-  newEntry.Dest   = e.backupdirectory
-  newEntry.Kind   = e.Options.Kind
-  newEntry.Type   = "inc"
-  newEntry.LastOplog  = lop
-  newEntry.FirstOplog = fop
-  newEntry.Compress   = e.Options.Compress
-  e.homeval.AddNewEntry(newEntry)
-  e.homeval.Flush()
+	newEntry := BackupEntry{}
+	newEntry.Id = backupId
+	newEntry.Ts = time.Now()
+	newEntry.Source = e.Options.Mongohost
+	newEntry.Dest = e.backupdirectory
+	newEntry.Kind = e.Options.Kind
+	newEntry.Type = "inc"
+	newEntry.LastOplog = lop
+	newEntry.FirstOplog = fop
+	newEntry.Compress = e.Options.Compress
+	e.homeval.AddNewEntry(newEntry)
+	e.homeval.Flush()
 
-  e.info.Printf("Success, %fMB of data has been saved in %s", size / (1024*1024), e.backupdirectory);
+	e.info.Printf("Success, %fMB of data has been saved in %s", size/(1024*1024), e.backupdirectory)
 }
--- mongobackup/copy.go
+++ mongobackup/copy.go
@@ -6,226 +6,230 @@
 **
 ** Started on  Thu 24 Dec 23:43:24 2015 gaspar_d
 ** Last update Mon  7 Mar 16:52:44 2016 gaspar_d
-*/
+ */
 
 package mongobackup
 
 import (
-  "os"
-  "io"
-  "strings"
-  "github.com/pierrec/lz4"
-  "github.com/allanhung/mongo-backup/utils"
+	"io"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/allanhung/mongo-backup/utils"
+	"github.com/pierrec/lz4"
 )
-
-
 
 // Copy a file to another destination
 // if the compress flag is present, compress the file while copying using lz4
 func (e *BackupEnv) CopyFile(source string, dest string) (err error, backedByte int64) {
-  sourcefile, err := os.Open(source)
-  if err != nil {
-    return err, 0
-  }
+	sourcefile, err := os.Open(source)
+	if err != nil {
+		return err, 0
+	}
 
-  defer sourcefile.Close()
+	defer sourcefile.Close()
 
-  var destfile io.Writer
-  if (e.Options.Compress) {
-    dest         += ".lz4"
-    dfile, err   := os.Create(dest)
-    if err != nil {
-      return err, 0
-    }
-    defer dfile.Close()
-    destfile = lz4.NewWriter(dfile)
-  } else {
-    dfile, err := os.Create(dest)
-    destfile   = dfile
-    if err != nil {
-      return err, 0
-    }
-    defer dfile.Close()
-  }
+	var destfile io.Writer
+	if e.Options.Compress {
+		dest += ".lz4"
+		dfile, err := os.Create(dest)
+		if err != nil {
+			return err, 0
+		}
+		defer dfile.Close()
+		destfile = lz4.NewWriter(dfile)
+	} else {
+		dfile, err := os.Create(dest)
+		destfile = dfile
+		if err != nil {
+			return err, 0
+		}
+		defer dfile.Close()
+	}
 
-  _, err = io.Copy(destfile, sourcefile)
-  if err != nil {
-    return err, 0
-  }
+	_, err = io.Copy(destfile, sourcefile)
+	if err != nil {
+		return err, 0
+	}
 
-  sourceinfo, err := os.Stat(source);
-  if err != nil {
-    return err, 0
-  }
+	sourceinfo, err := os.Stat(source)
+	if err != nil {
+		return err, 0
+	}
 
-  return nil, sourceinfo.Size();
+	return nil, sourceinfo.Size()
 }
 
 // Return the total size of the directory in byte
-func (e *BackupEnv) GetDirSize(source string) (int64) {
-  directory, _   := os.Open(source);
-  var sum int64   = 0;
-  defer directory.Close();
+func (e *BackupEnv) GetDirSize(source string) int64 {
+	directory, _ := os.Open(source)
+	var sum int64 = 0
+	defer directory.Close()
 
-  objects, _ := directory.Readdir(-1)
-  for _, obj := range objects {
-    if obj.IsDir() {
-      sum += e.GetDirSize(source + "/" + obj.Name());
-    } else {
-      stat, _ := os.Stat(source + "/" + obj.Name());
-      sum += stat.Size();
-    }
-  }
+	objects, _ := directory.Readdir(-1)
+	for _, obj := range objects {
+		if obj.IsDir() {
+			sum += e.GetDirSize(source + "/" + obj.Name())
+		} else {
+			stat, _ := os.Stat(source + "/" + obj.Name())
+			sum += stat.Size()
+		}
+	}
 
-  return sum;
+	return sum
 }
-
-
 
 // Copy a directory into another and compress all files if required
 func (e *BackupEnv) CopyDir(source string, dest string) (err error, backedByte int64) {
-  totalSize      := e.GetDirSize(source)
-  pb             := utils.ProgressBar{}
-  pb.Title        = "backup"
-  pb.Scale        = 3
-  err, _          = e.recCopyDir(source, dest, 0, totalSize, &pb)
+	totalSize := e.GetDirSize(source)
+	pb := utils.ProgressBar{}
+	pb.Title = "backup"
+	pb.Scale = 3
+	err, _ = e.recCopyDir(source, dest, 0, totalSize, &pb)
 
-  pb.End();
+	pb.End()
 
-  if err != nil {
-    return err, 0
-  }
+	if err != nil {
+		return err, 0
+	}
 
-  return nil, e.GetDirSize(dest)
+	return nil, e.GetDirSize(dest)
 }
 
+func (e *BackupEnv) TarDir(source string, dest string) (err error, backedByte int64) {
+	totalSize := e.GetDirSize(source)
+	t := time.Now()
+	destfilename := dest + "/" + e.Options.Prefix + "-" + t.Format("20060102") + ".tar.bz2.aes"
+	sh.Command("cd", source).Command("tar", "-cf", "-", "-j", ".").Command("openssl", "enc", "-e", "-aes-128-cbc", "-k", e.Options.EncPasswd, "-out", destfilename).Run()
+	stat, _ := os.Stat(destfilename)
+
+	return nil, stat.Size()
+}
 
 // Recursive copy directory function
 func (e *BackupEnv) recCopyDir(source string, dest string, backedByte int64, totalSize int64, pb *utils.ProgressBar) (err error, oBackedByte int64) {
-  sourceinfo, err := os.Stat(source);
+	sourceinfo, err := os.Stat(source)
 
-  if err != nil {
-    return err, 0;
-  }
+	if err != nil {
+		return err, 0
+	}
 
-  err = os.MkdirAll(dest, sourceinfo.Mode());
-  if err != nil {
-    return err, 0;
-  }
+	err = os.MkdirAll(dest, sourceinfo.Mode())
+	if err != nil {
+		return err, 0
+	}
 
-  directory, _ := os.Open(source)
-  objects, err := directory.Readdir(-1)
+	directory, _ := os.Open(source)
+	objects, err := directory.Readdir(-1)
 
-  for _, obj := range objects {
-    if (obj.Name() == "mongod.lock") {
-      continue;
-    }
+	for _, obj := range objects {
+		if obj.Name() == "mongod.lock" {
+			continue
+		}
 
-    sourcefilepointer      := source + "/" + obj.Name()
-    destinationfilepointer := dest + "/" + obj.Name()
+		sourcefilepointer := source + "/" + obj.Name()
+		destinationfilepointer := dest + "/" + obj.Name()
 
-    if obj.IsDir() {
-      err, backedByte  = e.recCopyDir(sourcefilepointer, destinationfilepointer, backedByte, totalSize, pb)
-      if err != nil {
-        e.error.Println(err)
-        return err, 0
-      }
-    } else {
-      err, size := e.CopyFile(sourcefilepointer, destinationfilepointer);
-      if err != nil {
-        e.error.Println(err);
-        return err, 0
-      }
-      backedByte = backedByte + size;
-      pb.Show(float32(backedByte) / float32(totalSize))
-    }
-  }
+		if obj.IsDir() {
+			err, backedByte = e.recCopyDir(sourcefilepointer, destinationfilepointer, backedByte, totalSize, pb)
+			if err != nil {
+				e.error.Println(err)
+				return err, 0
+			}
+		} else {
+			err, size := e.CopyFile(sourcefilepointer, destinationfilepointer)
+			if err != nil {
+				e.error.Println(err)
+				return err, 0
+			}
+			backedByte = backedByte + size
+			pb.Show(float32(backedByte) / float32(totalSize))
+		}
+	}
 
-  return nil, backedByte
+	return nil, backedByte
 }
-
 
 // restore & uncompress a backup to a specific location
 func (e *BackupEnv) RestoreCopyDir(entry *BackupEntry, source string, dest string, restoredByte int64, totalRestored int64, pb *utils.ProgressBar) (error, int64) {
-  directory, _  := os.Open(source)
-  objects, err  := directory.Readdir(-1)
+	directory, _ := os.Open(source)
+	objects, err := directory.Readdir(-1)
 
-  if err != nil {
-    return err, 0
-  }
+	if err != nil {
+		return err, 0
+	}
 
-  for _, obj := range objects {
-    sourcefilepointer      := source + "/" + obj.Name()
-    destinationfilepointer := dest + "/" + obj.Name()
-    if entry.Compress {
-      destinationfilepointer = strings.TrimSuffix(destinationfilepointer, ".lz4")
-    }
+	for _, obj := range objects {
+		sourcefilepointer := source + "/" + obj.Name()
+		destinationfilepointer := dest + "/" + obj.Name()
+		if entry.Compress {
+			destinationfilepointer = strings.TrimSuffix(destinationfilepointer, ".lz4")
+		}
 
-    if obj.IsDir() {
-      err,restoredByte = e.RestoreCopyDir(entry, sourcefilepointer, destinationfilepointer, restoredByte, totalRestored, pb)
-      if err != nil {
-        e.error.Println(err)
-        return err, 0
-      }
-    } else {
-      err, byteSource := e.RestoreCopyFile(sourcefilepointer, destinationfilepointer, entry)
-      restoredByte    += byteSource
-      pb.Show(float32(restoredByte) / float32(totalRestored))
-      if err != nil {
-        e.error.Println(err)
-        return err, 0
-      }
-    }
-  }
+		if obj.IsDir() {
+			err, restoredByte = e.RestoreCopyDir(entry, sourcefilepointer, destinationfilepointer, restoredByte, totalRestored, pb)
+			if err != nil {
+				e.error.Println(err)
+				return err, 0
+			}
+		} else {
+			err, byteSource := e.RestoreCopyFile(sourcefilepointer, destinationfilepointer, entry)
+			restoredByte += byteSource
+			pb.Show(float32(restoredByte) / float32(totalRestored))
+			if err != nil {
+				e.error.Println(err)
+				return err, 0
+			}
+		}
+	}
 
-  return nil, restoredByte
+	return nil, restoredByte
 }
-
 
 // Copy & Uncompress a specific file if required
 func (e *BackupEnv) RestoreCopyFile(source string, dest string, entry *BackupEntry) (error, int64) {
-  var (
-    sourcefile *os.File
-    destfile   *os.File
-    err        error
-    reader     io.Reader
-    writer     io.Writer
-  )
+	var (
+		sourcefile *os.File
+		destfile   *os.File
+		err        error
+		reader     io.Reader
+		writer     io.Writer
+	)
 
-  sourcefile, err = os.Open(source);
-  if err != nil {
-    return err, 0;
-  }
-  defer sourcefile.Close();
+	sourcefile, err = os.Open(source)
+	if err != nil {
+		return err, 0
+	}
+	defer sourcefile.Close()
 
-  if entry.Compress {
-    reader = lz4.NewReader(sourcefile)
-  } else {
-    reader = sourcefile
-  }
+	if entry.Compress {
+		reader = lz4.NewReader(sourcefile)
+	} else {
+		reader = sourcefile
+	}
 
-  destfile, err = os.Create(dest);
-  writer        = destfile
-  if err != nil {
-    return err, 0;
-  }
-  defer destfile.Close();
+	destfile, err = os.Create(dest)
+	writer = destfile
+	if err != nil {
+		return err, 0
+	}
+	defer destfile.Close()
 
-  _, err = io.Copy(writer, reader)
-  if err != nil {
-    return err, 0
-  }
+	_, err = io.Copy(writer, reader)
+	if err != nil {
+		return err, 0
+	}
 
-  sourceinfo, err := os.Stat(source);
-  if err != nil {
-    return err, 0
-  }
+	sourceinfo, err := os.Stat(source)
+	if err != nil {
+		return err, 0
+	}
 
-  return nil, sourceinfo.Size();
+	return nil, sourceinfo.Size()
 }
 
-
-func (e *BackupEnv) checkIfDirExist(dir string) (error) {
-  _, err := os.Stat(dir);
-  return err;
+func (e *BackupEnv) checkIfDirExist(dir string) error {
+	_, err := os.Stat(dir)
+	return err
 }
